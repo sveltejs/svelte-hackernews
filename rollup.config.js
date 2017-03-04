deb91b3258d0be44eb6ca4ef5485a4076cb253aa
Rich-Harris <richard.a.harris@gmail.com>
2017-03-03 23:08:18 -0500

    move rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -4,17 +4,21 @@ import commonjs from 'rollup-plugin-commonjs';
 import uglify from 'rollup-plugin-uglify';
 import buble from 'rollup-plugin-buble';
 
+const dev = !!process.env.DEVELOPMENT;
+
 export default {
 	entry: 'client/main.js',
 	dest: 'public/bundle.js',
 	format: 'iife',
 	plugins: [
-		nodeResolve({ jsnext: true }),
+		nodeResolve(),
 		commonjs(),
 		svelte({
-			css: process.env.DEVELOPMENT
+			// in development mode, we want to ship the CSS so that
+			// changes to components don't result in a hash mismatch
+			css: dev
 		}),
 		buble(),
-		uglify()
+		!dev && uglify()
 	]
 };
